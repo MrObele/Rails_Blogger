1c857fa28d1ee160c2c620c88a56fb8bbf1d3c67
mrobele <obeledavid@yahoo.com>
2018-08-11 15:53:04 +0100

    Sorcery authentication complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,18 +1,17 @@
 class TagsController < ApplicationController
-
+before_action :require_login, only: [:destroy]
 	def index
 		@tags = Tag.all
-
 		
 	end
 	def show
 		@tag = Tag.find(params[:id])
 		
 	end
-	# def destroy
-	# 	@tag = Tag.find(params[:id])
-	# 	@tag.destroy
- #        redirect_to tags_path
- #        flash.notice = "Tag '#{@tag.name}' Deleted!"
-	# end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+        redirect_to tags_path
+        flash.notice = "Tag '#{@tag.name}' Deleted!"
+	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
-	has_many :comments
-	has_many :taggings
-	has_many :tags, through: :taggings
+	has_many :comments, dependent: :delete_all 
+	has_many :taggings, dependent: :delete_all
+	has_many :tags, through: :taggings, dependent: :delete_all
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
@@ -17,7 +17,5 @@ class Article < ApplicationRecord
   new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
   self.tags = new_or_found_tags
 end
-
-
 
 end
